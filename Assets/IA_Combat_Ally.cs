139bacd5b83c60985f9f7824ce6b4bef5e315ad3
SamirRodrigues <emaildesamir@gmail.com>
2019-07-17 17:42:29 -0300

    17-07:17h42
    Alterações pequenas no Enemy, e acrescimo do Ally

---
+++ Assets/IA_Combat_Ally.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_Combat_Ally : MonoBehaviour
+{
+    public float hp = 100f;
+    public float atkDamage = 25f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
